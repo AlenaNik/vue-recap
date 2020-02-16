8ba157bca8960bfa3c7fe6dc13af159f9409b4bd
AlenaNik <alena.nik.digital@gmail.com>
2020-02-16 23:04:10 +0100

    random quote

--- src/components/Quote.vue
+++ src/components/Quote.vue
@@ -3,7 +3,7 @@
         <p>{{ quote }}</p>
        <div v-for="catquote in catquotes" :key="catquote"
        >{{ catquote }}</div>
-        <button @click="fetchCatFact">Random</button>
+        <button @click="fetchCatFact">Fetch random quote 🐈</button>
     </div>
 
 </template>
@@ -37,6 +37,7 @@ export default {
         font-family: ".SF NS Mono";
         text-align: center;
         color: black;
+        padding: 10px;
         font-size: 1em;
         height: 200px;
         width: auto;
@@ -47,6 +48,13 @@ export default {
         box-shadow: 2px -7px 23px -1px rgba(184,184,184,1);
         -moz-box-shadow: 2px -7px 23px -1px rgba(184,184,184,1);
         box-shadow: 2px -7px 23px -1px rgba(184,184,184,1);
+    }
+
+    button {
+        border-radius: 10px;
+        background-color: black;
+        padding: 5px 10px;
+        color: white
     }
 
 </style>
